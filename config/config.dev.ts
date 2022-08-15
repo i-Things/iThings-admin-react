733088aa8f3f71a668f22f9a6184c0ee9fa4f20b
huhui <569263082@qq.com>
2022-08-15 11:36:38 +0800

    chroe: 构建命令

--- config/config.dev.ts
+++ config/config.dev.ts
@@ -3,10 +3,8 @@ import { defineConfig } from 'umi';
 
 export default defineConfig({
   plugins: [
-    // https://github.com/zthxxx/react-dev-inspector
     'react-dev-inspector/plugins/umi/react-inspector',
   ],
-  // https://github.com/zthxxx/react-dev-inspector#inspector-loader-props
   inspectorConfig: {
     exclude: [],
     babelPlugins: [],
