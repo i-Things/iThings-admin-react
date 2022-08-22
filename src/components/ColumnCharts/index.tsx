2d7f6f57080387a0af97c74bcaf53fea39999f05
huhui <569263082@qq.com>
2022-08-22 11:09:58 +0800

    fix: 清除代码异味

--- src/components/ColumnCharts/index.tsx
+++ src/components/ColumnCharts/index.tsx
@@ -1,17 +1,11 @@
-import { Column } from "@ant-design/plots";
-import { Col, Empty, Row } from "antd";
-import React from "react";
-import { IColumnChartsCfg } from "./data";
+import { Column } from '@ant-design/plots';
+import { Col, Empty, Row } from 'antd';
+import React from 'react';
+import { IColumnChartsCfg } from './data';
 
-const ColumnCharts: React.FC<IColumnChartsCfg> = ({
-  data,
-  isGroup = false,
-  config,
-}) => {
+const ColumnCharts: React.FC<IColumnChartsCfg> = ({ data, isGroup = false, config }) => {
   const { data: udata, xField, yField } = data;
-  // console.log(data);
   const defaultConfig = {
-    // yAxis:{ max: 400, tickCount: 20 },
     data: udata,
     xField: xField,
     yField: yField,
@@ -24,11 +18,7 @@ const ColumnCharts: React.FC<IColumnChartsCfg> = ({
     <div style={{ marginBottom: 30 }}>
       <Row>
         <Col span={24}>
-          {mergeConfig?.data?.length == 0 ? (
-            <Empty />
-          ) : (
-            <Column {...mergeConfig} />
-          )}
+          {mergeConfig?.data?.length == 0 ? <Empty /> : <Column {...mergeConfig} />}
         </Col>
       </Row>
     </div>
