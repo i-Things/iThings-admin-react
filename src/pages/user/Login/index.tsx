3438ffa945198af652da2417fef7e456f639d799
huhui <569263082@qq.com>
2022-08-23 23:39:58 +0800

    fix: 更新默认账号密码

--- src/pages/user/Login/index.tsx
+++ src/pages/user/Login/index.tsx
@@ -1,30 +1,34 @@
-import {
-  postV1SystemUserCaptcha,
-  postV1SystemUserLogin
-} from "@/services/fmcsapi/yonghuguanli";
-import { apiParamsGUID, setToken, setUID } from "@/utils/utils";
-import {
-  FontColorsOutlined,
-  LockOutlined,
-  UserOutlined
-} from "@ant-design/icons";
-import { LoginForm, ProFormCaptcha, ProFormText } from "@ant-design/pro-form";
-import { Checkbox, Col, message, Row } from "antd";
-import React, { useEffect, useState } from "react";
-import { history, useModel } from "umi";
-import { loginType } from "./data";
-import styles from "./index.less";
+import { postV1SystemUserCaptcha, postV1SystemUserLogin } from '@/services/fmcsapi/yonghuguanli';
+import { apiParamsGUID, setToken, setUID } from '@/utils/utils';
+import { FontColorsOutlined, LockOutlined, UserOutlined } from '@ant-design/icons';
+import { LoginForm, ProFormCaptcha, ProFormText } from '@ant-design/pro-form';
+import { Checkbox, Col, message, Row } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { history, useModel } from 'umi';
+import type { loginType } from './data';
+import styles from './index.less';
 
 const Login: React.FC = () => {
-  const { initialState, setInitialState } = useModel("@@initialState");
-  const [captchaURL, setCaptchaURL] = useState<string>("");
-  const [codeID, setCodeID] = useState<string>("");
+  const { initialState, setInitialState } = useModel('@@initialState');
+  const [captchaURL, setCaptchaURL] = useState<string>('');
+  const [codeID, setCodeID] = useState<string>('');
 
   const fetchUserInfo = async () => {
     const userInfo = await initialState?.fetchUserInfo?.();
     if (userInfo) {
       await setInitialState((s) => ({ ...s, currentUser: userInfo }));
     }
+  };
+  const fetchCaptcha = async () => {
+    const prams = apiParamsGUID();
+    const body = {
+      type: 'sms',
+      use: 'login',
+    };
+    postV1SystemUserCaptcha(prams, body).then((res) => {
+      setCodeID(res?.data?.codeID ?? '');
+      setCaptchaURL(res?.data?.url ?? '');
+    });
   };
 
   const handleSubmit = async (values: loginType) => {
@@ -34,7 +38,7 @@ const Login: React.FC = () => {
         userID: values.userID,
         pwdType: 1,
         password: values.password,
-        loginType: "img",
+        loginType: 'img',
         code: values.code,
         codeID: codeID,
       };
@@ -42,10 +46,10 @@ const Login: React.FC = () => {
       // 登录
       const msg = await postV1SystemUserLogin(params, body);
       if (msg.data.token?.accessToken) {
-        const defaultLoginSuccessMessage = "登录成功！";
+        const defaultLoginSuccessMessage = '登录成功！';
         message.success(defaultLoginSuccessMessage);
         setToken(msg.data.token.accessToken);
-        setUID(msg?.data?.info?.uid ?? "");
+        setUID(msg?.data?.info?.uid ?? '');
 
         await fetchUserInfo();
         /** 此方法会跳转到 redirect 参数所在的位置 */
@@ -55,26 +59,14 @@ const Login: React.FC = () => {
         const { redirect } = query as {
           redirect: string;
         };
-        history.push(redirect || "/");
+        history.push(redirect || '/');
         return;
       }
     } catch (error) {
-      const defaultLoginFailureMessage = "登录失败，请重试！";
+      const defaultLoginFailureMessage = '登录失败，请重试！';
       message.error(defaultLoginFailureMessage);
       await fetchCaptcha();
     }
-  };
-
-  const fetchCaptcha = async () => {
-    const prams = apiParamsGUID();
-    const body = {
-      type: "sms",
-      use: "login",
-    };
-    postV1SystemUserCaptcha(prams, body).then((res) => {
-      setCodeID(res?.data?.codeID ?? "");
-      setCaptchaURL(res?.data?.url ?? "");
-    });
   };
 
   useEffect(() => {
@@ -91,8 +83,8 @@ const Login: React.FC = () => {
               <p className={styles.subTitle}>欢迎使用</p>
               <LoginForm
                 initialValues={{
-                  userID: "root123456",
-                  password: "fmcs123456",
+                  userID: 'administrator',
+                  password: 'iThings666',
                 }}
                 onFinish={async (values) => {
                   await handleSubmit(values as loginType);
@@ -101,57 +93,55 @@ const Login: React.FC = () => {
                 <ProFormText
                   name="userID"
                   fieldProps={{
-                    size: "large",
+                    size: 'large',
                     prefix: <UserOutlined className={styles.prefixIcon} />,
                   }}
-                  placeholder={"用户名: 17052709767"}
+                  placeholder={'用户名: 17052709767'}
                   rules={[
                     {
                       required: true,
-                      message: "用户名是必填项！",
+                      message: '用户名是必填项！',
                     },
                   ]}
                 />
                 <ProFormText.Password
                   name="password"
                   fieldProps={{
-                    size: "large",
+                    size: 'large',
                     prefix: <LockOutlined className={styles.prefixIcon} />,
                   }}
-                  placeholder={"密码: yl"}
+                  placeholder={'密码: yl'}
                   rules={[
                     {
                       required: true,
-                      message: "密码是必填项！",
+                      message: '密码是必填项！',
                     },
                   ]}
                 />
                 <ProFormCaptcha
                   countDown={1}
                   fieldProps={{
-                    size: "large",
-                    prefix: <FontColorsOutlined className={"prefixIcon"} />,
+                    size: 'large',
+                    prefix: <FontColorsOutlined className={'prefixIcon'} />,
                   }}
                   captchaProps={{
-                    size: "large",
+                    size: 'large',
                     className: styles.codeInput,
                   }}
-                  placeholder={"请输入验证码"}
+                  placeholder={'请输入验证码'}
                   captchaTextRender={() => {
-                    return (
-                      <img className={styles.captcha} src={captchaURL} alt="" />
-                    );
+                    return <img className={styles.captcha} src={captchaURL} alt="" />;
                   }}
                   name="code"
                   rules={[
                     {
                       required: true,
-                      message: "请输入验证码！",
+                      message: '请输入验证码！',
                     },
                   ]}
                   onGetCaptcha={async () => {
                     await fetchCaptcha();
-                    message.success("刷新验证码成功！");
+                    message.success('刷新验证码成功！');
                   }}
                 />
                 <div className={styles.mrb24}>
