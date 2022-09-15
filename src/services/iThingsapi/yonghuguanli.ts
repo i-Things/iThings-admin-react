dbdf0a3d34aa830573eab6125931c3e5b7f233d9
huhui <569263082@qq.com>
2022-09-15 16:48:39 +0800

    fix: 修改用户登录后的参数

--- src/services/iThingsapi/yonghuguanli.ts
+++ src/services/iThingsapi/yonghuguanli.ts
@@ -245,7 +245,7 @@ export async function postSystemUserLogin(
 export async function postSystemUserRead(
   body: {
     /** 用户id */
-    uid?: number;
+    uid?: string;
   },
   options?: { [key: string]: any },
 ) {
