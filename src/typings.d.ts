5301e9c42bbcee806323fdba033b72c14e3c62ad
oliver <153884673@qq.com>
2023-04-14 20:44:17 +0800

    fix(page): 优化ui逻辑

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -18,7 +18,8 @@ declare module 'crypto-js/md5';
 declare module 'mockjs';
 declare module 'react-fittext';
 declare module 'bizcharts-plugin-slider';
-
+declare module 'file-saver';
+declare module 'papaparse';
 // preview.pro.ant.design only do not use in your production ;
 // preview.pro.ant.design Dedicated environment variable, please do not use it in your project.
 declare let ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: 'site' | undefined;
