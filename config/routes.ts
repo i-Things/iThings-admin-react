1867f9a58148eda26dec814610a3b74ac58ad4b7
oliver <153884673@qq.com>
2023-09-13 12:30:36 +0800

    fix(routes): 更改重定向路由

--- config/routes.ts
+++ config/routes.ts
@@ -24,7 +24,7 @@ export default [
     routes: [
       {
         path: '/',
-        redirect: '/deviceMangers/product',
+        redirect: '/deviceMangers/product/index',
       },
       // {
       //   path: '/systemMangers',
